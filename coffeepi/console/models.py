182287c25cefdef62ba8b0d8a6bbeb788ed7c7d6
fuyo2 <hilliardcole@gmail.com>
2023-12-14 20:35:47 -0500

    Django Back-end updates
    Migrated DB into PostgreSQL.
    Created Model for Admin site to add new coffee to the database and to upload an image.

--- coffeepi/console/models.py
+++ coffeepi/console/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Coffee(models.Model):
+    upc = models.BigIntegerField()
+    brand = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    roast = models.CharField(max_length=255)
+    description = models.CharField(max_length=None)
+    image = models.ImageField(upload_to='images')
+
+    def __srt__(self):
+        return self.type
